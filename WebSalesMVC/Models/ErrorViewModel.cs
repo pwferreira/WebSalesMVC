31071f6f6088799e56501f223fb9a48e097f6f16
Patrick Ferreira <pwferreira.info@gmail.com>
2021-12-13 23:23:34 -0300

    Project Created

---
+++ WebSalesMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSalesMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
